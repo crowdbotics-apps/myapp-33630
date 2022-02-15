28c3b7fdea1ddfb704f84f4cf0c8bd1dad99ebf6
Crowdbotics <team@crowdbotics.com>
2022-02-15 18:08:58 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Food(models.Model):
+    "Generated Model"
+    name = models.BigIntegerField()
+    rating = models.IntegerField(
+        null=True,
+        blank=True,
+    )
